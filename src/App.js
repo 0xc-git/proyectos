a2ade553a3a12eb2ed48fb25d56a202e82a55bc1
fmgg <fmanuel.h@hotmail.com>
2022-05-03 18:57:04 +0200

    Start

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Sidebar from './components/sidebar';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar></Sidebar>
+    </div>
+  );
+}
+
+export default App;
